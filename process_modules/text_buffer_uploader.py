d79c76cb688b6c02c88d596862c958bc6c4d9208
hellosobik <hellosobik@gmail.com>
2024-10-21 19:15:01 +0530

    improvement of text_buffer is at maximum

--- process_modules/text_buffer_uploader.py
+++ process_modules/text_buffer_uploader.py
@@ -11,16 +11,17 @@ class Tbf:
         # self.disp_out.clear_display()
         buf=self.t_b.buffer()
         # print(buf)
-        for i in range(len(buf)):
+        # counter=0
+        ref_ar=self.t_b.ref_ar()
+        # print(ref_ar)
+        # for i in range(len(buf)):
+        for i in range(ref_ar[0]//self.t_b.cols, ref_ar[1]//self.t_b.cols):
             self.disp_out.set_page_address(i)
             self.disp_out.set_column_address(0)
             if buf[i].strip() != "":
 
                 for j in buf[i]:
                     chtr=j
-                    # print(buf[i])
-                    # print(chtr)
-                    # print(str(chtr))
                     chtr_byte_data=self.chrs.Chr2bytes(chtr)
                     for k in chtr_byte_data:
                         self.disp_out.write_data(k)
