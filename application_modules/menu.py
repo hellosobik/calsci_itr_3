974be1fa0f913db5d30649ce47cdd6bfd442f804
hellosobik <hellosobik@gmail.com>
2024-10-28 20:43:37 +0530

    menu branch created

---
+++ application_modules/menu.py
@@ -0,0 +1,7 @@
+from data_modules.characters import Characters
+from data_modules.keypad_map import Keypad_5X8
+from input_modules.keypad import Keypad
+from output_modules.st7565_spi import Display
+from process_modules.typer import Typer
+import utime as time  # type:ignore
+from math import *
